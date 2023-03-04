5475e08d373917f529889c8c2f39bb49521ee299
Abdilmajid Ibrahim <abdilwebdev@gmail.com>
2023-03-04 14:36:41 -0500

    some updates

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -11,7 +11,7 @@ function Home() {
     <div className="home">
       <div className="about">
       <img  src={Profile} alt="abdil" className="profile"/>
-        <h2> Hi, My Name is </h2>
+        <h2> Hi, My Name is Abdilmajid Ibrahim</h2>
         <div className="prompt">
           <p>A ... with a passion for learning and creating.</p>
           <LinkedInIcon />
@@ -25,20 +25,19 @@ function Home() {
           <li className="item">
             <h2> Front-End</h2>
             <span>
-              ReactJS, Angular, Redux, HTML, CSS, React Native, Flutter, NPM,
-              Ionic, BootStrap, MaterialUI, Yarn, TailwindCSS, StyledComponents
+              ReactJS, Redux, HTML, CSS, NPM, Ionic, BootStrap, MaterialUI, Yarn, TailwindCSS, StyledComponents
             </span>
           </li>
           <li className="item">
             <h2>Back-End</h2>
             <span>
-              NodeJS, Java Spring, .NET, ExpressJS, GraphQL, ApolloServer,
+              NodeJS, ExpressJS, GraphQL, ApolloServer,
               MySQL, MongoDB, DynamoDB, DigitalOcean, AWS S3, MS SQL
             </span>
           </li>
           <li className="item">
             <h2>Languages</h2>
-            <span>JavaScript, Java, Python, C#, C, C++, TypeScript, Go</span>
+            <span>JavaScript, Go</span>
           </li>
         </ol>
       </div>
