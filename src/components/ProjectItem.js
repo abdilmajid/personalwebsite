72b588d2fc4979860cac2c5a3a5025ae24f81901
Abdilmajid Ibrahim <abdilmajidibrahim@gmail.com>
2023-03-15 20:14:15 -0400

    fixed links to github and demo sites

--- src/components/ProjectItem.js
+++ src/components/ProjectItem.js
@@ -1,20 +1,19 @@
-import { RiExternalLinkLine } from "react-icons/ri";
 import { SiGithub } from "react-icons/si";
+// import { IoIosEye } from "react-icons/io";
+import Live from "../assets/Live1.svg"
 
 function ProjectItem({image,name,githublink,demolink}) {
   return (
     <div className='projectItem '>
       <div style={{ backgroundImage: `url(${image})` }} className="bgImage" />
         <h1> {name} </h1>
-        <div className="social">
 
-        <a href={githublink} >
+        <a href={githublink} className="social">
           <SiGithub/>
         </a>
-        <a href={demolink}>
-          <RiExternalLinkLine className="pulse"/>
+        <a href={demolink} className="live">
+          <img height="26" src={Live} alt="Demo Icon" className="social"/>
         </a>
-        </div>
     </div>
   );
 }
